6d2df52c47766ed2453c663e4591469e540271ef
Brian Lowe <mynameisboy@gmail.com>
2019-09-17 21:01:31 -0600

    something is broken

--- server/index.js
+++ server/index.js
@@ -1,57 +1,53 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const session = require('express-session')
-const {SERVER_PORT, SESSION_SECRET} = process.env
-const middleware = require('./middlewares/checkForSession')
-const swagCtrl = require('./controllers/swagController')
-const authCrl = require('./controllers/authController')
-const cartCtrl = require('./controllers/cartController')
-const searchCtrl = require('./controllers/searchController')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const {SERVER_PORT, SESSION_SECRET} = process.env;
+const middleware = require("./middlewares/checkForSession");
+const swagCtrl = require("./controllers/swagController");
+const authCrl = require("./controllers/authController");
+const cartCtrl = require("./controllers/cartController");
+const searchCtrl = require("./controllers/searchController");
 
 //middlewares
-
-app.use(express.static(`${__dirname}/../build`))
-app.use(express.json())
-app.use(session({
+app.use(express.json());
+app.use(
+  session({
     resave: false,
     saveUninitialized: false,
     secret: SESSION_SECRET,
     cookie: {
-        maxAge: 1000 * 60 * 60
-    }    
-}))
+      maxAge: 1000 * 60 * 60
+    }
+  })
+);
 
-
-app.use(middleware.checkForSession)
-
+app.use(middleware.checkForSession);
+app.use(express.static(`${__dirname}/../build`));
 
 //----- endpoints ------ //
 
 //swag
-app.get('/api/swag', swagCtrl.read)
+app.get("/api/swag", swagCtrl.read);
 
-app.get('/api/search', searchCtrl.search)
+app.get("/api/search", searchCtrl.search);
 
 //auth
-app.post('/api/login', authCrl.login)
+app.post("/api/login", authCrl.login);
 
-app.post('/api/register', authCrl.register)
+app.post("/api/register", authCrl.register);
 
-app.post('/api/signout', authCrl.signout)
+app.post("/api/signout", authCrl.signout);
 
-app.get('/api/user', authCrl.getUser)
+app.get("/api/user", authCrl.getUser);
 
 //cart
-app.post('/api/cart/checkout', cartCtrl.checkout)
+app.post("/api/cart/checkout", cartCtrl.checkout);
 
-app.post('/api/cart/:id', cartCtrl.add)
+app.post("/api/cart/:id", cartCtrl.add);
 
-app.delete('/api/cart/:id', cartCtrl.delete)
-
-
-
+app.delete("/api/cart/:id", cartCtrl.delete);
 
 app.listen(SERVER_PORT, () => {
-    console.log(`Listening on port ${SERVER_PORT}`)
-})
+  console.log(`Listening on port ${SERVER_PORT}`);
+});
